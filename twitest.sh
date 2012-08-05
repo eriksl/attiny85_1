8e99af6cdc53882430c1fdc622413533c71e27b7
Erik Slagter <erik@slagter.name>
2012-08-05 20:54:50 +0200

    Updated test script for pwm.

--- twitest.sh
+++ twitest.sh
@@ -3,6 +3,8 @@
 timeout=0.001
 flushcount=2
 
+testcase=2
+
 exec 6<> /dev/ttyUSB1
 
 function flush ()
@@ -21,19 +23,52 @@ function flush ()
 	done
 }
 
-while true
-do
-	for io in 0 1 2 3 2 1
-	do
-		command="6${io}"
+function hex ()
+{
 
-		echo "s 04 ${command} 01 p" >&6
+}
+
+case $testcase in
+	1)
+		while true
+		do
+			for io in 0 1 2 3 2 1
+			do
+				command="6${io}"
+
+				echo "s 04 ${command} 01 p" >&6
+				flush
+				echo "s 05 04 p" >&6
+				flush
+				echo "s 04 ${command} 00 p" >&6
+				flush
+				echo "s 05 04 p" >&6
+				flush
+			done
+		done
+	;;
+	2)
+		echo "s 04 p" >&6
 		flush
-		echo "s 05 04 p" >&6
-		flush
-		echo "s 04 ${command} 00 p" >&6
-		flush
-		echo "s 05 04 p" >&6
-		flush
-	done
-done
+
+		while true
+		do
+			for ((pwm = 1; pwm < 128; pwm += 5))
+			do
+				printf "w 80 %02x p\n" $[((pwm + 0) % 128) / 1] >&6
+				flush
+
+				printf "w 81 %02x p\n" $[((pwm + 64) % 128) / 1] >&6
+				flush
+
+				printf "w 82 %02x p\n" $[((192 - pwm) % 128) / 1] >&6
+				flush
+
+				printf "w 83 %02x p\n" $[((128 - pwm) % 128) / 1] >&6
+				flush
+
+				usleep 20000
+			done
+		done
+	;;
+esac
